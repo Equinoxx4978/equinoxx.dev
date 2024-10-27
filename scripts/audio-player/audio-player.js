2c85c7c876bc46f6e7a45704464b91928f0ff11d
Equinoxx4978 <joshuaridley07@gmail.com>
2024-10-27 13:15:35 +1100

    Added custom audio player

---
+++ scripts/audio-player/audio-player.js
@@ -0,0 +1,107 @@
+// Audio player script for the music player
+// Sourced from Martysh
+// https://512b.dev/msh/
+
+let trackListElement = document.getElementById("track-list");
+const nowPlayingElement = document.getElementById("now-playing");
+const playerElement = document.getElementById("player");
+const togglePlayingButton = document.getElementById("toggle-playing-button");
+const playerTimeElement = document.getElementById("player-time");
+
+let tracks = [];
+let nowPlayingIndex = null;
+
+function addTrack(trackName) {
+    let element = document.createElement("a");
+    element.classList.add("track-item");
+    element.textContent = (tracks.length + 1) + ". " + trackName;
+
+    let trackIndex = tracks.length;
+    element.onclick = function () {
+        playTrack(trackIndex);
+    };
+
+    trackListElement.append(element);
+
+    tracks.push({element: element, name: trackName, url: `music/${trackName}.mp3`});
+}
+
+function playTrack(index) {
+    tracks.forEach(function (track) {
+        track.element.classList.remove("selected-track");
+    });
+
+    const track = tracks[index];
+    track.element.classList.add("selected-track");
+
+    nowPlayingElement.textContent = track.name;
+
+    playerElement.src = track.url;
+    playerElement.load();
+    playerElement.play();
+
+    nowPlayingIndex = index;
+
+    console.log("playing track " + index + " - " + track.name)
+}
+
+function stop() {
+    nowPlayingElement.textContent = "nothing playing! :(";
+
+    tracks.forEach(function (track) {
+        track.element.classList.remove("selected-track");
+    });
+
+    playerElement.pause();
+    playerElement.src = "";
+    playerElement.currentTime = 0;
+}
+
+function togglePlaying() {
+    if (!playerElement.paused) {
+        playerElement.pause();
+        togglePlayingButton.textContent = "▶";
+    } else {
+        playerElement.play();
+        togglePlayingButton.textContent = "⏸";
+    }
+}
+
+function previous() {
+    if (nowPlayingIndex === 0) {
+        playTrack(tracks.length - 1);
+    } else {
+        playTrack(nowPlayingIndex - 1);
+    }
+}
+
+function next() {
+    if (nowPlayingIndex === tracks.length - 1) {
+        playTrack(0);
+    } else {
+        playTrack(nowPlayingIndex + 1);
+    }
+}
+
+playerElement.addEventListener("ended", function () {
+    if (nowPlayingIndex === tracks.length - 1) {
+        stop();
+    } else {
+        playTrack(nowPlayingIndex + 1);
+    }
+});
+
+playerElement.addEventListener("timeupdate", function () {
+    playerTimeElement.innerText = new Date(playerElement.currentTime * 1000).toISOString().substring(14, 19) + 
+        " / " + new Date(playerElement.duration * 1000).toISOString().substring(14, 19);
+
+});
+
+playerElement.addEventListener("play", function () {
+    togglePlayingButton.textContent = "⏸";
+});
+
+addTrack("Hven - iridESCEnT");
+addTrack("Exyl - How")
+
+stop();
